d794635c313953303cabb6bdf9b7e61bb7eab161
GitbyAxe1l <lroloff2007@gmail.com>
2025-10-22 22:38:06 +0200

    erster push das code da ist

---
+++ main.cpp
@@ -0,0 +1,117 @@
+/*
+ * Leon Roloff
+ * -----------------------------
+ * DVD Menu mit Switch case
+ * -----------------------------
+ */
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+
+    cout << "-------------DVD Menu------------\n"
+         << "|                               |\n"
+         << "|           [1] Start           |\n"
+         << "|           [2] Optionen        |\n"
+         << "|           [3] Auswahl         |\n"
+         << "|           [4] Exit            |\n"
+         << "|-------------------------------|\n"
+         << "| Nummer eingeben zum auswählen |\n"
+         << "---------------------------------\n";
+
+
+    char eingabe;
+    cin >> eingabe;
+
+    // Speicher für die menus
+
+    bool case1schleife = true;
+    bool case2schleife = true;
+    bool case3schleife = true;
+    bool pausiert = false;
+    string menueingaben;
+
+    switch (eingabe) {
+        case '1':
+
+            while (case1schleife) {
+                cout << "--------------Start--------------\n"
+                    << "|                               |\n"
+                    << "|                               |\n"
+                    << "|           * Film *            |\n"
+                    << "|                               |\n";
+                if (!pausiert) {
+                    cout << "|       * Spielt gerade *       |\n";
+                } else {
+                    cout << "|    * ist gerade Pausiert *    |\n";
+                }
+
+                cout << "|                               |\n"
+                     << "|                               |\n"
+                     << "---------------------------------\n";
+
+                if (pausiert) {
+                    cout << "|  'leertaste' wieder starten  |\n"
+                         << "|  'esc' zurük zum Hauptmenu   |\n";
+                } else {
+                    cout << "|    'leertaste' zum pausieren  |\n"
+                         << "|  'esc' zurük zum Hauptmenu    |\n";
+                }
+
+                cout << "|                               |\n"
+                     << "---------------------------------\n";
+                cin >> menueingaben;
+
+                if (menueingaben == "leertaste") {
+                    pausiert = !pausiert; // Umschalten zwischen Pause/Play
+                }
+                else if (menueingaben == "esc") {
+                    cout << "Zurück zum Hauptmenue...\n";
+                    case1schleife = false; // beende Start-Menü
+                }
+            }
+            break;
+
+        case '2':
+
+            while (case2schleife) {
+                cout << "--------------Optionen--------------\n"
+                    << "|                                    |\n"
+                    << "|           [1] Lautstärke           |\n"
+                    << "|           [2] Grafik               |\n"
+                    << "|           [3] Exit                 |\n"
+                    << "-------------------------------------\n";
+
+                cin >> menueingaben;
+
+                if (menueingaben == "esc") {
+                    break;
+                }
+            }
+
+            break;
+        case '3': while (case3schleife) {
+            cout << "--------------Auswahl--------------\n"
+                << "|                                    |\n"
+                << "|           [1] Lautstärke           |\n"
+                << "|           [2] Grafik               |\n"
+                << "|           [3] Exit                 |\n"
+                << "-------------------------------------\n";
+
+
+            if (menueingaben == "esc") {
+                break;
+            }
+        }
+        case '4': cout << "Cd wird ausgegeben";
+            break;
+
+
+        default: cout << "Option Menu" << endl;
+    }
+
+
+}
