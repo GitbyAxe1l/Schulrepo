2217efcf16bbe0f1ef9a144576eaf593a3daca65
GitbyAxe1l <lroloff2007@gmail.com>
2025-10-23 20:09:05 +0200

    2te aufgabe! mit if else DVD - Menu

--- main.cpp
+++ main.cpp
@@ -1,108 +1,56 @@
 /*
  * Leon Roloff
  * -----------------------------
- * DVD Menu mit Switch case
+ * DVD Menu mit if - else
  * -----------------------------
  */
 
 #include <iostream>
-#include <windows.h>
+#include <windows.h> //lybrary für die umstellung der Consolen.
 using namespace std;
 int main() {
+    //consolen umstellung das ich Ä Ü Ö verwenden kann
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
 
-    cout << "-------------DVD Menu------------\n"
-         << "|                               |\n"
-         << "|           [1] Start           |\n"
-         << "|           [2] Optionen        |\n"
-         << "|           [3] Auswahl         |\n"
-         << "|           [4] Exit            |\n"
-         << "|-------------------------------|\n"
-         << "| Nummer eingeben zum auswählen |\n"
-         << "---------------------------------\n";
-
-    /*
-     *-----------------------------------------------------------------------------
-     *                        Speicher für den ganzen Code
-     *-----------------------------------------------------------------------------
-    */
-    char eingabe;
-    cin >> eingabe;
-
-    // Speicher für die menus
-
-    bool case1schleife = true;
-    bool case2schleife = true;
-    bool case3schleife = true;
-    bool pausiert = false;
+    //Alle speicher für den Code!
+    int input;
     string menueingaben;
-    //-----------------------------------------------------------------------------
+    bool an = true;
 
 
-    switch (eingabe) {
-        case '1':
+    do{
+        cout << "-------------DVD Menu------------\n"
+           << "|                               |\n"
+           << "|           [1] Start           |\n"
+           << "|           [2] Optionen        |\n"
+           << "|           [3] Auswahl         |\n"
+           << "|           [4] Exit            |\n"
+           << "|-------------------------------|\n"
+           << "| Nummer eingeben zum auswählen |\n"
+           << "---------------------------------\n";
+        cin >> input;
 
-            while (case1schleife) {
-                cout << "--------------Start--------------\n"
+        if (input == 1) {
+            cout << "--------------Start--------------\n"
                     << "|                               |\n"
                     << "|                               |\n"
                     << "|           * Film *            |\n"
-                    << "|                               |\n";
-                if (!pausiert) {
-                    cout << "|       * Spielt gerade *       |\n";
-                } else {
-                    cout << "|    * ist gerade Pausiert *    |\n";
-                }
-
-                cout << "|                               |\n"
-                     << "|                               |\n"
-                     << "---------------------------------\n";
-
-                if (pausiert) {
-                    cout << "|  'leertaste' wieder starten  |\n"
-                         << "|  'esc' zurük zum Hauptmenu   |\n";
-                } else {
-                    cout << "|    'leertaste' zum pausieren  |\n"
-                         << "|  'esc' zurük zum Hauptmenu    |\n";
-                }
-
-                cout << "|                               |\n"
-                     << "---------------------------------\n";
-                cin >> menueingaben;
-
-                if (menueingaben == "leertaste") {
-                    pausiert = !pausiert; // Umschalten zwischen Pause/Play
-                }
-                else if (menueingaben == "esc") {
-                    cout << "Zurück zum Hauptmenue...\n";
-                    case1schleife = false; // beende Start-Menü
-                }
-            }
-            break;
-
-        case '2':
-
-            while (case2schleife) {
-                cout << "--------------Optionen--------------\n"
+                    << "|                               |\n"
+                    << "|       * Spielt gerade *       |\n"
+                    << "|                               |\n"
+                    << "|                               |\n"
+                    << "---------------------------------\n";
+        } else if (input == 2) {
+            cout << "--------------Optionen--------------\n"
                     << "|                                    |\n"
                     << "|           [1] Lautstärke           |\n"
                     << "|           [2] Grafik               |\n"
                     << "|           [3] Exit                 |\n"
                     << "-------------------------------------\n";
-
-                cin >> menueingaben;
-
-                if (menueingaben == "esc") {
-                    break;
-                }
-            }
-
-            break;
-        case '3':
-            while (case3schleife) {
-                cout << "--------------Auswahl--------------\n"
+        } else if (input == 3) {
+            cout << "--------------Auswahl--------------\n"
                     << "|                 |                 |\n"
                     << "|Film schnipsel 1 | Film schnipsel 2|\n"
                     << "|-----------------------------------|\n"
@@ -112,20 +60,15 @@ int main() {
                     << "|   gib 'esc' ein um zum Hauptmenü  |\n"
                     << "|             zu kommen|            |\n"
                     << "-------------------------------------\n";
+        } else if (input == 4) {
+            cout << "----------------Exit----------------\n"
+                 << "DVD wurde wieder ausgegeben";
+            an = false;
+        } else if (input <= 5) {
+            cout << "ungültige eingabe bitte wähle nur Nummern die auch zur Auswahl stehen\n";
 
-                cin >> menueingaben;
-                if (menueingaben == "esc") {
-                    case3schleife = false;
-
-                }
-            }
-            break;
-        case '4': cout << "Cd wird ausgegeben";
-            break;
-
-
-        default: cout << "Option Menu" << endl;
-    }
-
-
+        } else if (input == 0) {
+            cout << "ungültige eingabe bitte wähle nur Nummern die auch zur Auswahl stehen\n";
+        }
+    } while (an);
 }
