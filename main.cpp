a6f22c34b137ca77efaa115b793e181354dc9d48
leon <l.roloff2007@gmail.com>
2025-11-11 15:07:53 +0100

    hochladen des txt backups noten berechnung mit 100ter speicher

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
+/*
+ * Leon Roloff
+ *
+ */
+
 #include  <iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
 
-    int test [5] = { 16, 2, 77, 40, 12071 };
+    char eingabe;
+
+
+    float test[3]={6,6,6};
+
+    do {
+        cin >> eingabe;
+
+
+
+        switch (eingabe) {
+
+            case '1':
+                cout << "bitte gib 3 zahlen ein zum abspeichern in einem array\n";
+
+                for (int i =0; i < 3; i++) {
+                    test[i];
+                    cout << "Bitte gib die " << i +1 << " Zahl ein\n";
+                    cin >> test[i];
+                    cout << "Debug:" << test[i] << endl;
+                }
+
+                break;
+
+            case '2':
+
+                break;
+            default:
+                cout << "Falsche Eingabe!\n";
+                break;
+
+        }
+
+    } while (1);
+
+
+
+
+
 }
