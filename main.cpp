79c4ddba9d1005e1924eccd98c9a469830dbe98a
leon <l.roloff2007@gmail.com>
2025-11-11 18:37:05 +0100

    nur kurz commit das ich am rechner weiter machen kann

--- main.cpp
+++ main.cpp
@@ -12,13 +12,18 @@ int main() {
     SetConsoleCP(65001);
 
     char eingabe;
+    bool korrektur = false;
 
 
     float test[3]={6,6,6};
 
     do {
-        cin >> eingabe; //
 
+        cout << "[1] Manuelle Noteneingabe\n"
+             << "[2] Musgabe der manuellen Noteneingabe\n"
+             << "[3] Morrektur Möglichkeit der Noteneingabe\n";
+
+        cin >> eingabe; //
 
 
         switch (eingabe) {
@@ -37,11 +42,48 @@ int main() {
 
             case '2':
 
+                for (int i =0; i < 3; i++) {
+                    cout << "Note" << i+1 << " ist: " << test[i] << "\n";
+                }
+
+                break;
+
+            case '3':
+
+
+                if (eingabe == '1') {
+                    cout << "bastard";
+                }else if(eingabe == '2') {
+                    cout << "mongo";
+                } else if (eingabe == '3') {
+                    cout << "test3";
+                }
+                //korrektur = true;
+
+
                 break;
             default:
                 cout << "Falsche Eingabe!\n";
                 break;
 
+        }
+
+        while (korrektur == true) {
+
+
+
+            switch (eingabe) {
+                case '1':
+                    cout << "Geben sie bitte die zahl ein die sie an Stelle der ersten note ändern wollen;\n";
+                    break;
+                case '2':
+                    cout << "Geben sie bitte die zahl ein die sie an Stelle der ersten note ändern wollen;\n";
+                    break;
+                case '3':
+                    cout << "Geben sie bitte die zahl ein die sie an Stelle der ersten note ändern wollen;\n";
+                default:
+                    break;
+            }
         }
 
     } while (1);
