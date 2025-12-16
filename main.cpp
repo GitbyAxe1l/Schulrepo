b6b45645def85679c104fb05634cdc5d4e42dd64
leonl <lroloff2007@gmail.com>
2025-12-16 22:12:32 +0100

    Widerstands berechnungs umsetzung von einem Struktogramm

--- main.cpp
+++ main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
-#include <vector>
 #include <windows.h>
-#include <iomanip>
 using namespace std;
 
+float parallel(int r1,int r2);
+float reihen(int r1,int r2);
+
 int main() {
+
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
 
+    int dummy1;
+    int dummy2;
+    float rges =0;
+    int dummy3;
 
+
+    cout << "Geben sie  bitte die Werte für die Widerstände R1 und R2 ein" << endl;
+
+    cin>>dummy1;
+    cin>>dummy2;
+
+    cout << "Wähle sie die Schaltungsweise um Rgesamt auszurechnen" << endl
+         << "Paralell [1] , Reihe [2]" << endl;
+
+    cin >> dummy3;
+
+    switch (dummy3) {
+        case 1:
+            rges = parallel(dummy1,dummy2);
+            cout <<  "Dein Gesammter Widerstand ist: " << rges << endl;
+            break;
+        case 2:
+            rges = reihen(dummy1,dummy2);
+            cout <<  "Dein Gesammter Widerstand ist: " << rges << endl;
+            break;
+        default:
+            cout<<"du Säckel"<<endl;
+            break;
+    }
+}
+float parallel(int r1,int r2) {
+    float rges;
+    //cout << r1 << " " << r2 << endl;
+    rges = 1.0 / ( (1.0 / r1) + (1.0 / r2) );
+    return rges;
+}
+float reihen(int r1,int r2) {
+    //cout << r1 << " " << r2 << endl;
+    return r1+r2;
 }
 
-
-// Setw ist einfach nur ausrichtung für die nächste ausgabe
-// left ist nur die ausrichtung so wie right oder internal für vorzeichen
