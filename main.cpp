ec53968afc7563f7240c111e2a360368b6ba76c5
leonl <lroloff2007@gmail.com>
2026-01-23 09:03:59 +0100

    structs

--- main.cpp
+++ main.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 
 
+string testen (string namenodersowas = "Ayri");
+
 
 //
 int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
+    testen("mohamed");
+    testen();
 
 
 
     return 0;
+}
+
+
+string testen (string namenodersowas) {
+    cout << namenodersowas;
 }
