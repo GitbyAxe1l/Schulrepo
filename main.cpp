ccd7b7a6228f72cb567fe4102f813523b89045dc
leonl <lroloff2007@gmail.com>
2026-02-12 16:03:59 +0100

    structs

--- main.cpp
+++ main.cpp
@@ -2,73 +2,114 @@
 #include <windows.h>
 using namespace std;
 
-struct SD { // Spielerdaten
-    string nn = "User";
-    int lev = 0;
-    int pts = 0;
-
-    void setspielerdaten(int spielerNr);
-    void getspielerdaten(int spielerNr) const;
+struct WSR {
+    // Funktion für manuelle eingabe fehlt
+    float median[3];
+    int n = 10;
+    float boxplot[10]={5,5,3,5,1,1,3,2,2,1};
+    void bubblesort (void);
+    void bubblesortausgabe(void);
+    void medianberechnung (void);
+    void MinumumMaximum(void);
+    void mittelwert(void);
+    void trennungslinie(void);
 };
 
-// Eingabe der Spielerdaten (für EINEN Spieler)
-void SD::setspielerdaten(int spielerNr) {
-    cout << "Bitte geben sie ein Username für Spieler " << spielerNr << endl;
-    cin >> nn;
+void WSR::medianberechnung (void) {
 
-    cout << "Bitte geben sie das Level für Spieler " << spielerNr << endl;
-    cin >> lev;
+    int median1stelle;
+    int median2stelle;
+    int median3stelle;
 
-    cout << "Bitte geben sie die Punkte für Spieler " << spielerNr << endl;
-    cin >> pts;
+    //ungerade braucht  nen fix aber gerade kein bock
+    if (n % 2 == 0) {
+         //Mittlere Median Stelle
+        int median1= n/2;
+        int median2= median1/2;
+        int median3= median1 + ((n-median1)/2);
+        //1 2 3 4 5 6 7 8 9 10
+        //   q     q     q
+
+        median[0] = (boxplot[median2] + boxplot[median2 + 1])/2;
+        cout <<boxplot[median2] <<" , "<<boxplot[median2 + 1];
+        median[1] = (boxplot[median1-1] + boxplot[median1])/2;
+        median[2] = boxplot[median3] + boxplot[median3+ 1]/2;
+
+        for (int i = 0; i <3; i++) {
+            cout << "Dein Median [" << i +1 << "] hat den Wert: " <<median[i] << endl;
+        }
+    }else {
+    //Abspeichern in median array fehlt
+        trennungslinie();
+        median1stelle = n /2;
+        cout << "median 1 ist an Stelle= " << median1stelle +1 << "\nder Wert an dieser Stelle ist " <<boxplot[median1stelle] << endl;
+        trennungslinie();
+        median2stelle =  (median1stelle - 1)/2;
+        cout << "median 2 ist an Stelle= " << median2stelle +1 << "\nder Wert an dieser Stelle ist " <<boxplot[median2stelle] << endl;
+        trennungslinie();
+        median3stelle =  median1stelle + ((n - median1stelle)/2);
+        cout << "median 3 ist an Stelle= " << median3stelle +1 << "\nder Wert an dieser Stelle ist " <<boxplot[median3stelle] << endl;
+        trennungslinie();
+
+    }
+
+}
+void WSR::trennungslinie (void) {
+    cout << "----------------------------------------\n";
 }
 
-// Ausgabe der Spielerdaten (für EINEN Spieler)
-void SD::getspielerdaten(int spielerNr) const {
-    cout << "\nDer Spieler " << spielerNr << " hat folgende Daten:\n";
-    cout << "Username: " << nn << endl;
-    cout << "Level: " << lev << endl;
-    cout << "Punkte: " << pts << endl;
+void WSR::MinumumMaximum(void) {
+    trennungslinie();
+    cout << "Dein Minimum ist " << boxplot[0] << "\ndein Maximum ist: " << boxplot[n-1] << endl;
+    trennungslinie();
+
+}
+
+
+void WSR::mittelwert() {
+    float mittelwert = 0;
+    for (int i = 0; i < n; i++) {
+        mittelwert += boxplot[i];
+    }
+    mittelwert /= 2;
+    trennungslinie();
+    cout << "Dein Mittelwert ist " << mittelwert << endl;
+    trennungslinie();
+}
+
+void WSR::bubblesort (void) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (boxplot[j] > boxplot[j + 1]) {
+                int temp = boxplot[j];
+                boxplot[j] = boxplot[j + 1];
+                boxplot[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void WSR::bubblesortausgabe(void) {
+    cout << "\n--- Bubblesort ausgabe---\n";
+    for (int i = 0; i < n; i++) {
+        cout << boxplot[i] << " ";
+    }
+    cout << "\n-------------------------\n";
+
 }
 
 int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    SD Spieler[5];
+    //SetConsoleOutputCP(1252);
 
-    // Eingabe
-    for (int i = 0; i < 5; i++) {
-        Spieler[i].setspielerdaten(i + 1);
-    }
-
-    // Ausgabe
-    for (int i = 0; i < 5; i++) {
-        Spieler[i].getspielerdaten(i + 1);
-    }
-
-
-
-    int n = 5;
-    // Wer hat die meisten Punkte -- Bubblesort
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (Spieler[j].lev < Spieler[j + 1].lev) {
-                SD temp = Spieler[j];
-                Spieler[j] = Spieler[j + 1];
-                Spieler[j + 1] = temp;
-            }
-        }
-    }
-
-    // Ausgabe: Ranking nach Level
-    cout << "\n--- Ranking Nach LVL---\n";
-    for (int i = 0; i < n; i++) {
-        cout << (i + 1) << ". " << Spieler[i].nn
-             << " | Level: " << Spieler[i].lev
-             << " | Punkte: " << Spieler[i].pts << endl;
-    }
+    WSR Mwerte;
+    Mwerte.bubblesort();
+    Mwerte.bubblesortausgabe();
+    Mwerte.medianberechnung();
+    Mwerte.MinumumMaximum();
+    Mwerte.mittelwert();
 
 
     return 0;
